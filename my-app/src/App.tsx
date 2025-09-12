f6d61f9ddfc0befe45e5af7b7ff7063af43f4f19
AbdulRehman247136 <su92-bssem-f23-094@superior.edu.pk>
2025-09-12 20:08:40 +0500

    changes in the app.tsx enabled protected routes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,30 @@
-
 import './App.css'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import SignUp from './page/signup/SignUp.tsx';
 import Login from './page/Login/Login.tsx';
 import MainPage from './page/mainpage/MainPage.tsx';
-
+import { useSelector } from 'react-redux';
+import type { RootState } from './State/store/Store.ts';
 
 function App() {
-
+  const token = useSelector((state: RootState) => state.auth["access-token"]);
 
   return (
-  <Router>
-    <Routes>
-     <Route path='/' element={<MainPage/>}/>
-      <Route path='/Signup' element={<SignUp/>}/>
-      <Route path='/Login' element={<Login/>}/>
-    </Routes>
-     
+    <Router>
+      <Routes>
+        {/* Protected route: only accessible with token */}
+        <Route
+          path="/"
+          element={token ? <MainPage /> : <Navigate to="/login" />}
+        />
+
+        {/* Public routes */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </Router>
   )
 }
 
-
-export default App
+export default App;
